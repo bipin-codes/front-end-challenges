c30afe962773f090146a754229ab8d6ecbaac288
bipingosain <bipincodes@outlook.com>
2022-06-27 20:17:13 +0530

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { ButtonPage } from "./pages/ButtonPage";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ButtonPage } from "./pages/Buttons/ButtonPage";
+import { Index } from "./pages/Base";
+import { InputPage } from "./pages/Input/InputPage";
 export const App: React.FC = () => {
   return (
-    <>
-      <ButtonPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />}>
+          <Route path="/button" element={<ButtonPage />}></Route>
+          <Route path="/input" element={<InputPage />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
---
+++ src/pages/Input/InputPage.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export const InputPage: React.FC = () => {
+  return <h1>Will host Input Page soon!</h1>;
+};
