bc9c3993e563b2603a68645c52c00c0983a46b42
Bipin Gosain <bipincodes@outlook.com>
2022-07-09 07:25:32 +0530

    Navbar completed

--- src/components/WindBnB/Navbar/Navbar.tsx
+++ src/components/WindBnB/Navbar/Navbar.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../assets/images/logo.png";
 import "./Navbar.css";
 const Navbar = () => {
+  const [guests, setGuests] = useState(undefined);
+  const [location, setLocation] = useState("Helsinki, Finland");
+
   return (
     <nav className="navbar_container">
       <div className="navbar_container_left">
@@ -11,10 +14,25 @@ const Navbar = () => {
       </div>
       <div className="navbar_container_right">
         <div className="search-parent">
-          <input type="search" placeholder="Search" className="" />
-          <input type="search" placeholder="Search" />
+          <input
+            readOnly
+            type="search"
+            placeholder="Search"
+            className="search-box"
+            value={location}
+          />
+          <div className="vl"></div>
+          <input
+            readOnly
+            type="search"
+            placeholder="Add guests"
+            className="search-box search-box-bordered"
+            value={guests}
+          />
+          <div className="vl"></div>
+
+          <span className="material-icons icon"> {"search"}</span>
         </div>
-        <button type="submit">Search</button>
       </div>
     </nav>
   );
